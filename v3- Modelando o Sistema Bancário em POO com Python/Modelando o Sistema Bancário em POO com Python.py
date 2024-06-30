7a9aaec4327a2a361ba1c4d188a9452304c1679c
Vinicius Mendes <121885868+M3ndezz@users.noreply.github.com>
2024-06-30 01:16:29 -0300

    Add files via upload

---
+++ v3- Modelando o Sistema Bancário em POO com Python/Modelando o Sistema Bancário em POO com Python.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+
+class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+
+    def adicionar_conta(self, conta):
+        self.contas.append(conta)
+
+class PessoaFisica:
+    def __init__(self, nome, data_nascimento, cpf, endereco):
+        super().__init__(endereco)
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+
+class Conta:
+    def __init__(self, numero, cliente):
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = "0001"
+        self._cliente = cliente
+        self._historico = Historico()
+
+class ContaCorrente:
+    def __init__(self):
+        pass
+
+class Historico:
+    def __init__(self):
+        pass
+
+class Transacao:
+    def __init__(self):
+        pass
+
+class Deposito:
+    def __init__(self):
+        pass
+
+class Saque:
+    def __init__(self):
+        pass
